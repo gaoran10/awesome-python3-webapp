1562b636026fac029c96608335f4a66415a6b9e6
= <=>
2017-08-11 18:02:00 +0800

    config

---
+++ www/config_default.py
@@ -0,0 +1,14 @@
+# 默认配置文件，用于开发环境
+
+configs = {
+    'db': {
+        'host': '123.206.178.243',
+        'port': 3306,
+        'user': 'admin',
+        'password': 'Admin123123',
+        'database': 'awesome'
+    },
+    'session': {
+        'secret': 'AwEsOmE'
+    }
+}
